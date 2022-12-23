b050c979994ef8db78bb46b78b7e8f94b4e5f162
hirabayashimana <hbmn5415@gmail.com>
2022-12-23 21:54:40 +0900

    file up

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Vue_TodoList
 
-Vue3の練習を兼ねてToDoListのアプリを作成しました。
+Vue3 の練習を兼ねて ToDoList のアプリを作成しました。
 
-・配列にpush()とsplice()で入力した要素の追加・削除。<br>
-・v-on:clickでイベントハンドラ<br>
+・配列に push()と splice()で入力した要素の追加・削除。<br>
+・v-on:click でイベントハンドラ<br>
 ・v-for()in~で入力した要素をリストとして表示<br>
 
-※パッケージマネージャーはyarn
-nodemoduleは除いてあるのでターミナルで[yarn]をする。
-ローカルサーバー立ち上げは、[yarn dev]
+※パッケージマネージャーは yarn
+nodemodule は除いてあるのでターミナルで<em>yarn</em>をする。<br>
+ローカルサーバー立ち上げは、<em>yarn dev</em>
---
+++ src/TestView.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { reactive, ref } from "vue";
+
+const state = reactive({
+  todo: "",
+  todos: [],
+});
+const addTodo = () => {
+  state.todos.push(state.todo);
+  state.todo = "";
+};
+const removeTodo = (index) => state.todos.splice(index, 1);
+
+const color = ref("mediumblue");
+</script>
+
+<template>
+  <div id="app">
+    <h1>Todoリスト</h1>
+    <input v-model="state.todo" /><br />
+    <button @click="addTodo">追加</button>
+    <ul>
+      <li v-for="(todo, index) in state.todos" :key="todo">
+        <span>{{ index + 1 }}</span
+        >{{ todo }}
+        <span v-bind:style="{ color: color }" @click="removeTodo(index)"
+          >×</span
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+#app {
+  max-width: 400px;
+  margin: 0 auto;
+  text-align: center;
+}
+h1 {
+  color: darkorchid;
+
+  letter-spacing: 0.1rem;
+  font-size: 32px;
+}
+button {
+  margin-top: 20px;
+  background-color: #888;
+  border: none;
+  color: #fff;
+  padding: 5px 10px;
+  border-radius: 3px;
+}
+ul {
+  display: block;
+  text-align: center;
+  padding: 0 80px;
+}
+li {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+  border-bottom: 1px solid #f6f6f6;
+  padding-bottom: 5px;
+}
+li span {
+  margin: 0 10px;
+}
+</style>
